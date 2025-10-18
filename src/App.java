3d9c4165ae82dc2e3a787646bd1e6c5b5ecf9f5a
hortuadev <hortuadev@gmail.com>
2025-10-18 17:47:26 +0100

    fix(App.java): corregida correctamente la estructura del metodo runProcess que recorre el directorio input y crea procesos con los nombres de los archivos pasados como parametros al metodo de invocacion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.io.File;
+
+import net.salesianos.Process.ProcessLauncher;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runProcess();
+
     }
+
+    public static void runProcess() {
+
+        File directory = new File("./src/net/salesianos/Files/input/");
+        String[] files = directory.list();
+
+        for (String file : files) {
+            String fileName = file.replace(".csv", "");
+
+            ProcessLauncher.createProcess(fileName);
+        }
+
+    }
+
 }

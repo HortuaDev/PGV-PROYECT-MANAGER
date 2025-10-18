5923710c000e0527260a39b371414e73ef32de00
hortuadev <hortuadev@gmail.com>
2025-10-18 18:03:11 +0100

    fix(App.java): creado una lista que almacena los processo y les agrega el waitFor(), para esperar a que todos terminen para verificar que los archivos organizados esten creados

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.salesianos.Process.ProcessLauncher;
 
@@ -13,12 +15,23 @@ public class App {
         File directory = new File("./src/net/salesianos/Files/input/");
         String[] files = directory.list();
 
+        List<Process> processList = new ArrayList<>();
+
         for (String file : files) {
             String fileName = file.replace(".csv", "");
 
-            ProcessLauncher.createProcess(fileName);
+            processList.add(ProcessLauncher.createProcess(fileName));
         }
 
+        for (Process process : processList) {
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Procesos terminados");
     }
 
 }

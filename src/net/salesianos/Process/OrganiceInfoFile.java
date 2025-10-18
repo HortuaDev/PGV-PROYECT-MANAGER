2e7dbb6adeb5556cb02532e150c55c33ec3c742d
hortuadev <hortuadev@gmail.com>
2025-10-18 17:53:47 +0100

    fix(OrganiceInfoFile.java): se creo la estructura del archivo correctamente, el funcionamiento es leer el archivo correspondiente .csv, obtener sus lineas y convertirlas en objetos Product, se agregan a un List<Product>, se ordena por categoria, y luego se imprime pro consola los datos de la lista organizada, (se guardan en el archivo del directorio /output)

---
+++ src/net/salesianos/Process/OrganiceInfoFile.java
@@ -0,0 +1,59 @@
+package net.salesianos.Process;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.salesianos.Object.Product;
+
+public class OrganiceInfoFile {
+    public static void main(String[] args) {
+
+        String fileName = args[0];
+        List<Product> products = new java.util.ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(
+                new FileReader("./src/net/salesianos/Files/input/" + fileName + ".csv"));) {
+
+            String line = "";
+            boolean firstLine = true;
+            // Obtengo la informacion del fichero y la combierto a objetos Product
+
+            while ((line = br.readLine()) != null) {
+
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
+
+                String[] attributes = line.split(",");
+
+                products.add(new Product(Integer.parseInt(attributes[0]), attributes[1], attributes[2],
+                        Float.parseFloat(attributes[3]), Float.parseFloat(attributes[4])));
+            }
+
+            // ordeno la lista por categoría
+
+            Collections.sort(products, new Comparator<Product>() {
+                @Override
+                public int compare(Product p1, Product p2) {
+                    return p1.getCategoria().compareTo(p2.getCategoria());
+                }
+            });
+
+            // escribo la lista ordenada en el fichero de salida
+            for (Product p : products) {
+                System.out
+                        .println(p.getId() + "," + p.getNombre() + "," + p.getCategoria() + "," + p.getCantidad() + ","
+                                + p.getPrecio());
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error al procesar el fichero: " + fileName + ".: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}

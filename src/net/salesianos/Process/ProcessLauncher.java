64ce5caa5823fc7b6c2e13db28c0c19538f8f8e8
hortuadev <hortuadev@gmail.com>
2025-10-18 17:50:49 +0100

    fix(ProcessLauncher.java): creado el metodo estatico (CreateProcess), el cual se esncarga de crear los processos y recibe como parametro el nombre del archivo, debido a problemas con la ejecucion del ProcessBuilder, la ruta del archivo para ejecutar el programa, se cambio al archivo .class de la clase OrganiceInfoFile, porque al parecer, aveces genera conflictos y el programa no se ejecutaba correctamente, pero con esto si funciona sin problema alguno

--- src/net/salesianos/Process/ProcessLauncher.java
+++ src/net/salesianos/Process/ProcessLauncher.java
@@ -1,5 +1,28 @@
 package net.salesianos.Process;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ProcessLauncher {
+
+    public static Process createProcess(String fileName) {
+        try {
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-cp", "./bin",
+                    "net.salesianos.Process.OrganiceInfoFile",
+                    fileName);
+
+            pb.redirectOutput(new File("./src/net/salesianos/Files/output/" + fileName + ".csv"));
+
+            return pb.start();
+
+        } catch (IOException e) {
+            System.out.println("Error al iniciar el proceso: " + fileName + ".: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }

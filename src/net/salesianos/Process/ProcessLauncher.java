502bdc2d88a5bddd2e57930c3d46ec35f1f05dd2
hortuadev <hortuadev@gmail.com>
2025-10-19 14:24:18 +0100

    fix(ProcessLauncher.java): creado el metodo categoryclasificationProcess, con finalidad de leer cada uno de los archivos ya filtrados en /output y unirlos todos en un solo archivo FinalFile.scv, (no arganizado por categoria, solo concatenando toda la informacion de los archivos)

--- src/net/salesianos/Process/ProcessLauncher.java
+++ src/net/salesianos/Process/ProcessLauncher.java
@@ -25,4 +25,24 @@ public class ProcessLauncher {
         return null;
     }
 
+    public static Process categoryClasificationProcess(String fileName) {
+        try {
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-cp", "./bin",
+                    "net.salesianos.Process.CategoryClasification",
+                    fileName);
+
+            pb.redirectOutput(ProcessBuilder.Redirect.appendTo(
+                    new File("./src/net/salesianos/Files/organizedFiles/FinalFile.csv")));
+
+            return pb.start();
+
+        } catch (IOException e) {
+            System.out.println("Error al iniciar el proceso: " + fileName + ".: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
